c5ad1b7ef5a853f2de4acff7b09052bba93e2fc3
ArshiMahajan22 <77196068+ArshiMahajan22@users.noreply.github.com>
2021-08-12 12:51:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup() {
   canvas = createCanvas(250, 250);
-  canvas.position(200, screen.width/2);
+  canvas.position(300, screen.width/2);
   video = createCapture(VIDEO);
   video.hide();
 
