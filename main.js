98fd00794aa94c328177fc56a7cd14621d96be7d
ArshiMahajan22 <77196068+ArshiMahajan22@users.noreply.github.com>
2021-08-14 08:56:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup() {
   canvas = createCanvas(250, 250);
-  canvas.position(screen.width/2, screen.height/2);
+  canvas.position(510, 335);
   video = createCapture(VIDEO);
   video.hide();
 
